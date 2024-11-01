738357e0f437f282c816fc9755e8b11ae5b5ac46
wuyihung <wuyihung7916@gmail.com>
2024-11-01 15:23:21 +0800

    Test Bazel formatting check

--- chapter_1/exercise_1_1/test/test.cc
+++ chapter_1/exercise_1_1/test/test.cc
@@ -5,13 +5,11 @@ extern "C" {
 }
 
 TEST(Exercise_1_1, PrintfOutput) {
-    testing::internal::CaptureStdout();
+  testing::internal::CaptureStdout();
 
   exercise_1_1();
 
   std::string output = testing::internal::GetCapturedStdout();
 
   EXPECT_EQ(output, "hello, world\n");
-
-  
 }
