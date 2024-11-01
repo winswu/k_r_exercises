f4d532c5b2edd3c42c85a37f53a39f3f4d632ab1
wuyihung <wuyihung7916@gmail.com>
2024-11-01 12:06:20 +0800

    Add exercise 1-1

---
+++ chapter_1/exercise_1_1/solution/lib.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int exercise_1_1()
+{
+	printf("hello, world\n");
+	return 0;
+}
---
+++ chapter_1/exercise_1_1/test/test.cc
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+
+extern "C" {
+#include "../solution/lib.h"
+}
+
+TEST(Exercise_1_1, PrintfOutput) {
+  testing::internal::CaptureStdout();
+
+  exercise_1_1();
+
+  std::string output = testing::internal::GetCapturedStdout();
+
+  EXPECT_EQ(output, "hello, world\n");
+}
